1152c5d72fc4419a6ecc86ca5db70338fbda194e
jzchen <jzchen@cecgw.cn>
2018-09-05 17:06:53 +0800

    删除多余文件

--- create_db.py
+++
@@ -1,4 +0,0 @@
-from database import db
-from models.user import User
-from models.yasi_info import YasiInfo
-db.create_all()
--- database.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from application import app
-
-db = SQLAlchemy(app)
